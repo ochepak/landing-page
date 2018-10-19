31761bbb4cd34b6a2c76b8aebb9141d547703684
Oleh Chepak <o.chepak@outlook.com>
2018-10-20 02:23:40 +0300

    second commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,24 @@
 window.onload = () => {
+
     let images = document.querySelectorAll('img');
 
     images.forEach(img => {
         img.onmousedown = (e) => e.preventDefault();
     });
+
+    document.getElementById('to-top').onclick = () => {
+        document.body.scrollTop = 0; 
+        document.documentElement.scrollTop = 0;
+    }
+}
+
+window.onscroll = () => {
+    scrollFunction();
+    function scrollFunction() {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            document.getElementById("to-top").style.display = "block";
+        } else {
+            document.getElementById("to-top").style.display = "none";
+        }
+    }
 }
